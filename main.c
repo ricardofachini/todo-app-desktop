4efe7af35a7b7c05020539a3ce37b16938a4af42
Ricardo Fachini <ricardofachinisouza@gmail.com>
2024-07-31 22:54:17 -0300

    initial glfw window

--- main.c
+++ main.c
@@ -1,4 +1,27 @@
+#include <GLFW/glfw3.h>
+#include "stdio.h"
+
 
 int main() {
-    return 0;
+    GLFWwindow* main_window;
+
+    if (!glfwInit()) {
+        printf("Erro ao inicializar a janela");
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
+    if (!window) {
+        // Window or OpenGL context creation failed
+        glfwTerminate(); // encerra a janela
+        return -1;
+    }
+
+    
+
+
+
+
+
+    glfwTerminate(); // encerra a janela
 }
