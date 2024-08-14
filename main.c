1703e48c3071907240afb95955a3ea8bd1c14c68
Ricardo Fachini <ricardofachinisouza@gmail.com>
2024-08-14 11:47:35 -0300

    estilizacao botao

--- main.c
+++ main.c
@@ -45,6 +45,29 @@ int main() {
                 true);
         lf_text("Lista de tarefas");
 
+        {
+          static const float width = 160.0f;
+
+          lf_set_ptr_x_absolute(win_width - width - WIN_MARGIN * 2.0f);
+          LfUIElementProps props = lf_get_theme().button_props;
+
+
+          props.margin_left = 0.0f;
+          props.margin_right = 0.0f;
+          // estilização botao
+          props.color = (LfColor){65, 167, 204, 255};
+          props.border_width = 0.0f;
+          props.corner_radius = 4.0f;
+
+
+
+          lf_push_style_props(props);
+          lf_set_line_should_overflow(false);
+          lf_button_fixed("Nova tarefa", width, -1);
+          lf_set_line_should_overflow(true);
+          lf_pop_style_props();
+        }
+
         lf_end();
 
 
